9323a9c18270777d8ae187e08f55c0d50e84e87e
Marcus Behel <marcus.behel@mindspring.com>
2022-01-03 22:03:36 -0500

    Added raspicam pipeline

--- README.md
+++ README.md
@@ -12,20 +12,37 @@ Low latency, real-time camera streaming using a Raspberry Pi.
 sudo apt install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-omx-rpi-config gstreamer1.0-omx-rpi gstreamer1.0-omx
 ```
 
+libcamera buster: config.txt (after installing)
+
+```
+# In [all] section
+dtoverlay=vc4-fkms-v3d
+camera_auto_detect=1
+```
+
+Otherwise, must use old stack (raspicam). Recommended to increase gpu memory (should be at least 128, but 256 better, but don't use 256 on Pi 3A+)
+
+```
+gpu_mem=256
+```
+
 
 ## Scripts
 TODO
 
 ## Playing Stream
 
-- Install ffmpeg and run one of the following commands
+- Playing with [mpv](https://mpv.io/) (recommended). Make sure to change the fps argument to match your stream if needed.
 
-```
-ffplay -fflags nobuffer -flags low_delay -framedrop -sync ext tcp://remote_host:5008
-
-ffplay -fflags nobuffer -flags low_delay -framedrop -sync ext udp://localhost:5008
-
-# Add -probesize 32 to reduce startup time
+```sh
+mpv --no-cache --untimed --profile=low-latency -no-correct-pts --fps=30 --osc=no tcp://remote_host:5008
 ```
 
-TODO: mpv, mplayer, and vlc
+- Using ffplay (part of [ffmpeg](https://ffmpeg.org/)). This method may have latency issues (depends on the OS you are using and how the stream from the Pi is created). It is recommended to use this as a fallback if mpv does not work or has latency issues.
+
+```sh
+ffplay -probesize 32 -framerate 30 -fflags nobuffer -flags low_delay -framedrop -sync ext tcp://remote_host:5008
+```
+
+
+TODO: mplayer and vlc
--- stream.py
+++ stream.py
@@ -31,18 +31,18 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Starts a stream using the Pi camera using the newer camera stack (libcamera). The system must be configured for the new camera stack (if using buster or older).")
-    parser.add_argument("--driver", type=str, choices=["libcamera"], default="libcamera", help="Which input device driver to use. V4L2 supports USB cameras. Libcamera and raspicam only support the pi camera modules (libcamera being the new stack). Default = libcamera")
+    parser.add_argument("--driver", type=str, choices=["libcamera", "raspicam"], default="libcamera", help="Which input device driver to use. V4L2 supports USB cameras. Libcamera and raspicam only support the pi camera modules (libcamera being the new stack). Default = libcamera")
     parser.add_argument("--device", type=str, default="", help="Device identifier for which camera to use (does not apply to libcamera or raspicam).")
     parser.add_argument("--width", type=int, default=640, help="Width of the video stream (must be a supported resolution). Default = 640")
     parser.add_argument("--height", type=int, default=480, help="Height of the video stream (must be a supported resolution). Default = 480")
     parser.add_argument("--framerate", type=int, default=30, help="Video stream framerate (must be supported for the active resolution). Default = 30")
     parser.add_argument("--format", metavar="FORMAT", type=str, choices=["h264", "mjpeg"], default="h264", help="Format for the stream (h264 or mjpeg). Generally, H.264 is lower bandwidth, while MJPEG is lower latency. Default = h264")
     parser.add_argument("--bitrate", type=int, default=2048000, help="Bitrate for H.264 stream (does not apply to MJPEG). Default = 2048000")
-    parser.add_argument("--profile", metavar="PROFILE", type=str, choices=["baseline", "main", "high"], default="baseline", help="Profile for H.264 stream (does not apply to MJPEG) Either baseline, main, or high. Default = baseline")
-    parser.add_argument("--quality", metavar="QUALITY", type=int, choices=range(1, 100), default=50, help="JPEG image quality (1-100) for MPEG stream (does not apply to H.264). Default = 50")
+    parser.add_argument("--profile", metavar="PROFILE", type=str, choices=["baseline", "main", "high"], default="baseline", help="Profile for H.264 stream (does not apply to MJPEG, unless using raspicam driver) Either baseline, main, or high. Default = baseline")
+    parser.add_argument("--quality", metavar="QUALITY", type=int, choices=range(1, 100), default=50, help="JPEG image quality (1-100) for MPEG stream (does not apply to H.264, does not work with raspicam driver). Default = 50")
     parser.add_argument("--vflip", action='store_true', help="Vertically flip the camera image.")
     parser.add_argument("--hflip", action='store_true', help="Vertically flip the camera image.")
-    parser.add_argument("--rotate", metavar="ROTATION", type=int, choices=[0, 90, 180, 270, 360], default=0, help="Rotate camera image (0, 90, 180, 270, 360). Default = 0")
+    parser.add_argument("--rotate", metavar="ROTATION", type=int, choices=[0, 90, 180, 270], default=0, help="Rotate camera image (0, 90, 180, 270). Default = 0")
     parser.add_argument("--port", type=int, default=5008, help="Which TCP port to use.")
     res = parser.parse_args()
     
@@ -53,10 +53,22 @@ if __name__ == "__main__":
         # The libcamerasrc gstreamer plugin could be used, but does not appear to support the options libcamera-vid does
         cmd = "libcamera-vid -t 0 --inline --width {width} --height {height} --framerate {framerate} " \
                 "--codec {format} --bitrate {bitrate} --profile {profile} --quality {quality} " \
-                " {vflip} {hflip} --rotation {rotate} -o - | " \
+                " {vflip} {hflip} --rotation {rotate} -n -o - | " \
                 "gst-launch-1.0 fdsrc fd=0 ! tcpserversink host=0.0.0.0 port={port}".format(
                     width=res.width, height=res.height, framerate=res.framerate, format=res.format, 
                     bitrate=res.bitrate, profile=res.profile, quality=res.quality, port=res.port, 
+                    rotate=res.rotate, vflip=("--vflip" if res.vflip else ""), hflip=("--hflip" if res.hflip else ""))
+    elif res.driver == "raspicam":
+        # Use raspicam (old stack) to capture frames from the camera (more optimized for the pi camera than v4l2)
+        # Then use gstreamer to create a tcp server
+        # raspivid does support a tcp server, however it exists after the first client disconnects
+        # The raspicamsrc gstreamer plugin could be used, but does not appear to be easily installed on buster
+        cmd = "raspivid -t 0 --inline --width {width} --height {height} --framerate {framerate} " \
+                "--codec {format} --bitrate {bitrate} --profile {profile} " \
+                " {vflip} {hflip} --rotation {rotate} -n -o - | " \
+                "gst-launch-1.0 fdsrc fd=0 ! tcpserversink host=0.0.0.0 port={port}".format(
+                    width=res.width, height=res.height, framerate=res.framerate, format=res.format.upper(), 
+                    bitrate=res.bitrate, profile=res.profile, port=res.port, 
                     rotate=res.rotate, vflip=("--vflip" if res.vflip else ""), hflip=("--hflip" if res.hflip else ""))
     print(cmd)
     os.system(cmd)
