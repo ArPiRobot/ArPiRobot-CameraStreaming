16728928fdbcb91b653fcd8fa3eb32a9fc26063e
Marcus Behel <marcus.behel@mindspring.com>
2022-01-04 00:27:30 -0500

    Working on v4l2 stream

--- README.md
+++ README.md
@@ -9,7 +9,13 @@ Low latency, real-time camera streaming using a Raspberry Pi.
 - Install gstreamer
 
 ```sh
-sudo apt install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-omx-rpi-config gstreamer1.0-omx-rpi gstreamer1.0-omx
+sudo apt install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl
+```
+
+For supporting the OMX encoder also run
+
+```sh
+sudo apt install gstreamer1.0-omx-rpi-config gstreamer1.0-omx-rpi gstreamer1.0-omx
 ```
 
 libcamera buster: config.txt (after installing)
@@ -23,7 +29,7 @@ camera_auto_detect=1
 Otherwise, must use old stack (raspicam). Recommended to increase gpu memory (should be at least 128, but 256 better, but don't use 256 on Pi 3A+)
 
 ```
-gpu_mem=256
+gpu_mem=128
 ```
 
 
@@ -32,17 +38,24 @@ TODO
 
 ## Playing Stream
 
-- Playing with [mpv](https://mpv.io/) (recommended). Make sure to change the fps argument to match your stream if needed.
+- Playing with [gstreamer](https://gstreamer.freedesktop.org/) (recommended for mjpeg).
 
 ```sh
-mpv --no-cache --untimed --profile=low-latency -no-correct-pts --fps=30 --osc=no tcp://remote_host:5008
+# Playing mjpeg stream
+gst-launch-1.0 tcpclientsrc host=remote_host port=5008 ! jpegdec ! autovideosink
+
+# Playing h264 stream (will likely be some latency)
+gst-launch-1.0 tcpclientsrc host=remote_host port=5008 ! h264parse ! avdec_h264 ! autovideosink
 ```
 
-- Using ffplay (part of [ffmpeg](https://ffmpeg.org/)). This method may have latency issues (depends on the OS you are using and how the stream from the Pi is created). It is recommended to use this as a fallback if mpv does not work or has latency issues.
+- Playing with [mpv](https://mpv.io/) (recommended for h264). Make sure to change the fps argument to match your stream if needed (you can also try setting it to a higher value to reduce latency). MPV does not seem to work with mjpeg streams.
 
 ```sh
-ffplay -probesize 32 -framerate 30 -fflags nobuffer -flags low_delay -framedrop -sync ext tcp://remote_host:5008
+mpv --no-cache --untimed --profile=low-latency -no-correct-pts --fps=60 --osc=no tcp://remote_host:5008
 ```
 
+- Using ffplay (part of [ffmpeg](https://ffmpeg.org/)). This method may have latency issues (depends on the OS you are using and how the stream from the Pi is created). It is recommended to use this as a fallback if mpv does not work or has latency issues. You can also try setting framerate to 60 even if the source framerate is lower to help latency (this may ensure the newest frame is always displayed).
 
-TODO: mplayer and vlc
+```sh
+ffplay -probesize 32 -framerate 60 -fflags nobuffer -flags low_delay -framedrop -sync ext tcp://remote_host:5008
+```
--- stream.py
+++ stream.py
@@ -31,19 +31,21 @@ import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Starts a stream using the Pi camera using the newer camera stack (libcamera). The system must be configured for the new camera stack (if using buster or older).")
-    parser.add_argument("--driver", type=str, choices=["libcamera", "raspicam"], default="libcamera", help="Which input device driver to use. V4L2 supports USB cameras. Libcamera and raspicam only support the pi camera modules (libcamera being the new stack). Default = libcamera")
+    parser.add_argument("--driver", type=str, choices=["libcamera", "raspicam", "v4l2"], default="libcamera", help="Which input device driver to use. V4L2 supports USB cameras. Libcamera and raspicam only support the pi camera modules (libcamera being the new stack). Default = libcamera")
     parser.add_argument("--device", type=str, default="", help="Device identifier for which camera to use (does not apply to libcamera or raspicam).")
+    parser.add_argument("--iomode", metavar="IOMODE", type=str, default="auto", choices=["auto", "rw", "mmap", "userptr", "dmabuf", "dmabuf-import"], help="IO mode for v4l2 devices. If latency issues exist with USB webcam try using dmabuf. Default = auto")
     parser.add_argument("--width", type=int, default=640, help="Width of the video stream (must be a supported resolution). Default = 640")
     parser.add_argument("--height", type=int, default=480, help="Height of the video stream (must be a supported resolution). Default = 480")
     parser.add_argument("--framerate", type=int, default=30, help="Video stream framerate (must be supported for the active resolution). Default = 30")
     parser.add_argument("--format", metavar="FORMAT", type=str, choices=["h264", "mjpeg"], default="h264", help="Format for the stream (h264 or mjpeg). Generally, H.264 is lower bandwidth, while MJPEG is lower latency. Default = h264")
-    parser.add_argument("--bitrate", type=int, default=2048000, help="Bitrate for H.264 stream (does not apply to MJPEG). Default = 2048000")
-    parser.add_argument("--profile", metavar="PROFILE", type=str, choices=["baseline", "main", "high"], default="baseline", help="Profile for H.264 stream (does not apply to MJPEG, unless using raspicam driver) Either baseline, main, or high. Default = baseline")
+    parser.add_argument("--bitrate", type=int, default=2048000, help="Bitrate for H.264 stream (does not apply to MJPEG, unless using raspicam driver). Default = 2048000")
+    parser.add_argument("--profile", metavar="PROFILE", type=str, choices=["baseline", "main", "high"], default="baseline", help="Profile for H.264 stream (does not apply to MJPEG) Either baseline, main, or high. Default = baseline")
     parser.add_argument("--quality", metavar="QUALITY", type=int, choices=range(1, 100), default=50, help="JPEG image quality (1-100) for MPEG stream (does not apply to H.264, does not work with raspicam driver). Default = 50")
     parser.add_argument("--vflip", action='store_true', help="Vertically flip the camera image.")
     parser.add_argument("--hflip", action='store_true', help="Vertically flip the camera image.")
     parser.add_argument("--rotate", metavar="ROTATION", type=int, choices=[0, 90, 180, 270], default=0, help="Rotate camera image (0, 90, 180, 270). If using libcamera, only 0 and 180 are supported. Default = 0")
     parser.add_argument("--port", type=int, default=5008, help="Which TCP port to use.")
+    parser.add_argument("--gain", type=float, default=10.0, help="Configure the gain of the camera (supported with libcamera and raspicam). Affects brightness of the images. Default = 10.0")
     res = parser.parse_args()
     
     if res.driver == "libcamera":
@@ -53,22 +55,57 @@ if __name__ == "__main__":
         # The libcamerasrc gstreamer plugin could be used, but does not appear to support the options libcamera-vid does
         cmd = "libcamera-vid -t 0 --inline --width {width} --height {height} --framerate {framerate} " \
                 "--codec {format} --bitrate {bitrate} --profile {profile} --quality {quality} " \
-                " {vflip} {hflip} --rotation {rotate} -n -o - | " \
+                " {vflip} {hflip} --rotation {rotate} --gain {gain} -n -o - | " \
                 "gst-launch-1.0 fdsrc fd=0 ! tcpserversink host=0.0.0.0 port={port}".format(
                     width=res.width, height=res.height, framerate=res.framerate, format=res.format, 
                     bitrate=res.bitrate, profile=res.profile, quality=res.quality, port=res.port, 
-                    rotate=res.rotate, vflip=("--vflip" if res.vflip else ""), hflip=("--hflip" if res.hflip else ""))
+                    rotate=res.rotate, vflip=("--vflip" if res.vflip else ""), hflip=("--hflip" if res.hflip else ""),
+                    gain=res.gain)
     elif res.driver == "raspicam":
         # Use raspicam (old stack) to capture frames from the camera (more optimized for the pi camera than v4l2)
         # Then use gstreamer to create a tcp server
         # raspivid does support a tcp server, however it exists after the first client disconnects
         # The raspicamsrc gstreamer plugin could be used, but does not appear to be easily installed on buster
+
+        # Gain is two controls with raspivid: analog and digital
+        # Maximum analog gain (experimentally) seems to be 15
+        # Use analog gain as much as possible. Use digital gain when required gain is too large for analog alone
+        analog_gain = min(res.gain, 15.0)
+        digital_gain = res.gain / analog_gain
+
         cmd = "raspivid -t 0 --inline --width {width} --height {height} --framerate {framerate} " \
                 "--codec {format} --bitrate {bitrate} --profile {profile} " \
-                " {vflip} {hflip} --rotation {rotate} -n -o - | " \
+                " {vflip} {hflip} --rotation {rotate} --drc off --digitalgain {dg} --analoggain {ag} -n -o - | " \
                 "gst-launch-1.0 fdsrc fd=0 ! tcpserversink host=0.0.0.0 port={port}".format(
                     width=res.width, height=res.height, framerate=res.framerate, format=res.format.upper(), 
                     bitrate=res.bitrate, profile=res.profile, port=res.port, 
-                    rotate=res.rotate, vflip=("--vflip" if res.vflip else ""), hflip=("--hflip" if res.hflip else ""))
+                    rotate=res.rotate, vflip=("--vflip" if res.vflip else ""), hflip=("--hflip" if res.hflip else ""),
+                    dg=digital_gain, ag=analog_gain)
+    elif res.driver == "v4l2":
+        # Gain is two controls with raspivid: analog and digital
+        # Maximum analog gain (experimentally) seems to be 15
+        # Use analog gain as much as possible. Use digital gain when required gain is too large for analog alone
+        analog_gain = min(res.gain, 15.0)
+        digital_gain = res.gain / analog_gain
+
+        # Apply settings using v4l2-ctl
+        # Some cameras may not support all settings
+        # Note: When using new camera stack (libcamera) Pi camera will not work properly via v4l2. Many settings are unavailable
+        os.system("v4l2-ctl -d {0} --set-ctrl=rotate={1}".format(res.device, res.rotate))
+        os.system("v4l2-ctl -d {0} --set-ctrl=horizontal_flip={1}".format(res.device, 1 if res.hflip else 0))
+        os.system("v4l2-ctl -d {0} --set-ctrl=vertical_flip={1}".format(res.device, 1 if res.vflip else 0))
+        os.system("v4l2-ctl -d {0} --set-ctrl=analogue_gain={1}".format(res.device, analog_gain))
+        os.system("v4l2-ctl -d {0} --set-ctrl=digital_gain={1}".format(res.device, digital_gain))
+
+        if res.format == "h264":
+            enc = "x264enc tune=zerolatency speed-preset=ultrafast bitrate={0} ! video/x-h264,profile={1} ! h264parse config-interval=-1".format(int(res.bitrate / 1000.0), res.profile)
+        elif res.format == "mjpeg":
+            enc = "jpegenc quality={0}".format(res.quality)
+
+        cmd = "gst-launch-1.0 v4l2src device={device} io-mode={iomode} ! " \
+                "video/x-raw,width={width},height={height},framerate={framerate}/1 ! " \
+                "{enc} ! tcpserversink host=0.0.0.0 port={port}".format(device=res.device, iomode=res.iomode, 
+                    width=res.width, height=res.height, framerate=res.framerate, enc=enc, port=res.port)
+
     print(cmd)
     os.system(cmd)
